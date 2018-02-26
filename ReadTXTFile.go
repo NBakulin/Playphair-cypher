7c8fc20f913cc6abb06196b1efc91aeb13a5ca63
Nikita Bakulin <bakulin.nikita@bk.ru>
2018-02-26 10:08:14 +0500

    frequency data now stores into json file

--- ReadTXTFile.go
+++ ReadTXTFile.go
@@ -6,7 +6,31 @@ import (
 	"strings"
 	"regexp"
 	"unicode"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
 )
+var frequencyArrayFileName = "frequencyArray.json"
+var txtFileName = "text.txt"
+func readFile (fileName string) string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return ""
+	}
+	defer file.Close()
+
+	fileSize, err := file.Stat()
+	if err != nil {
+		return ""
+	}
+
+	bs := make([]byte, fileSize.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		return ""
+	}
+	return string(bs)
+}
 
 func getRuneNumber(singleRune rune) int32 {
 	if unicode.IsLetter(singleRune) {
@@ -31,39 +55,52 @@ func getRuneNumber(singleRune rune) int32 {
 	return 0
 }
 
-func main() {
-	file, err := os.Open("text.txt")
+func createFrequencyArrayFile() {
+	file, err := os.Create(frequencyArrayFileName)
 	if err != nil {
 		return
 	}
 	defer file.Close()
+}
 
-	fileSize, err := file.Stat()
-	if err != nil {
-		return
-	}
-
-	bs := make([]byte, fileSize.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
-	inputFileString := string(bs)
+func main() {
+	//read file
+	inputFileString := readFile(txtFileName)
+	//regex to remove non-russian letters and four other characters
 	var regex = regexp.MustCompile("[^а-яА-Я\\-,:.]*")
+	//apply regex on string with replacing spaces
 	changedFileString := strings.Replace(regex.ReplaceAllString(inputFileString, ""), " ", "", -1)
-	var charsArray [][]int
+	//make all lower case ones
 	changedFileString = strings.ToLower(changedFileString)
+	//initialised map for frequency analysis and inserted nulls into it
+	charsArray := map[int64][36]int64{}
+	var nullsArray [36]int64
 	for i := 0; i < 36; i++ {
-		charsArray = append(charsArray, []int{})
-		for j := 0; j < 36; j++ {
-			charsArray[i] = append(charsArray[i], 0)
-		}
+		charsArray[int64(i)] = nullsArray
 	}
+	//check if file exists and put there empty array if file is empty
+	if readFile(frequencyArrayFileName) == "" {
+		createFrequencyArrayFile()
+		emptyFrequencyJson, _ := json.Marshal(charsArray)
+		ioutil.WriteFile(frequencyArrayFileName, emptyFrequencyJson, 0644)
+	}
+	//reads json from file
+	jsonfile, err := ioutil.ReadFile(frequencyArrayFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	var jsonobject map[int64][]int64
+	json.Unmarshal(jsonfile, &jsonobject)
+	fmt.Printf("%v", jsonobject)
+
 	var runesArray = []rune(changedFileString)
 	for i := 0; i < len(runesArray) - 1; i++ {
 		var firstBigrammLetter = getRuneNumber(runesArray[i])
 		i++
 		var secondBigrammLetter = getRuneNumber(runesArray[i])
-		charsArray[firstBigrammLetter][secondBigrammLetter]++
+		jsonobject[int64(firstBigrammLetter)][secondBigrammLetter]++
 	}
+		fullFrequencyJson, _ := json.Marshal(jsonobject)
+		ioutil.WriteFile(frequencyArrayFileName, fullFrequencyJson, 0644)
 }
